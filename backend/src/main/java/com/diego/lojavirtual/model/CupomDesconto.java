7b1a5d33c3c1466f8be37c21394b6cfe6d167b7d
Diego Oliveira <diegodsoliveira@gmail.com>
2022-09-26 19:02:19 -0300

    classe Cupom de desconto

---
+++ backend/src/main/java/com/diego/lojavirtual/model/CupomDesconto.java
@@ -0,0 +1,78 @@
+package com.diego.lojavirtual.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "cupom_desconto")
+@SequenceGenerator(name = "seq_cupom_desconto", sequenceName = "seq_cupom_desconto", allocationSize = 1, initialValue = 1)
+public class CupomDesconto implements Serializable {
+    private static final long serialVersionUID = -6063297776735443845L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_cupom_desconto")
+    private Long id;
+
+    private String codigoCupom;
+    private BigDecimal valorRealDesconto;
+    private BigDecimal valorPorcentagemDesconto;
+
+    @Temporal(TemporalType.DATE)
+    private Date validadeCupom;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCodigoCupom() {
+        return codigoCupom;
+    }
+
+    public void setCodigoCupom(String codigoCupom) {
+        this.codigoCupom = codigoCupom;
+    }
+
+    public BigDecimal getValorRealDesconto() {
+        return valorRealDesconto;
+    }
+
+    public void setValorRealDesconto(BigDecimal valorRealDesconto) {
+        this.valorRealDesconto = valorRealDesconto;
+    }
+
+    public BigDecimal getValorPorcentagemDesconto() {
+        return valorPorcentagemDesconto;
+    }
+
+    public void setValorPorcentagemDesconto(BigDecimal valorPorcentagemDesconto) {
+        this.valorPorcentagemDesconto = valorPorcentagemDesconto;
+    }
+
+    public Date getValidadeCupom() {
+        return validadeCupom;
+    }
+
+    public void setValidadeCupom(Date validadeCupom) {
+        this.validadeCupom = validadeCupom;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CupomDesconto)) return false;
+        CupomDesconto that = (CupomDesconto) o;
+        return getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
