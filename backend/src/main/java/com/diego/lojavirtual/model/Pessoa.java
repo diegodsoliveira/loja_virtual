689ba478a3d0cbadfcc66b438d7b0d3182c1fa7c
Diego Oliveira <diegodsoliveira@gmail.com>
2022-09-26 15:02:55 -0300

    classes Pessoa, física e jurídica

---
+++ backend/src/main/java/com/diego/lojavirtual/model/Pessoa.java
@@ -0,0 +1,66 @@
+package com.diego.lojavirtual.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@SequenceGenerator(name = "seq_pessoa", sequenceName = "seq_pessoa", allocationSize = 1, initialValue = 1)
+public abstract class Pessoa implements Serializable {
+
+    private static final long serialVersionUID = -2379746061320388122L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_pessoa")
+    private Long Id;
+
+    private String nome;
+    private String email;
+    private String telefone;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pessoa)) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return getId().equals(pessoa.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
---
+++ backend/src/main/java/com/diego/lojavirtual/model/PessoaJuridica.java
@@ -0,0 +1,67 @@
+package com.diego.lojavirtual.model;
+
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pessoa_juridica")
+@PrimaryKeyJoinColumn(name = "id")
+public class PessoaJuridica extends Pessoa {
+    private static final long serialVersionUID = 1471400271051147236L;
+
+    private String cnpj;
+    private String inscricaoEstadual;
+    private String inscricaoMunicipall;
+    private String nomeFantasia;
+    private String razaoSocial;
+    private String categoria;
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getInscricaoEstadual() {
+        return inscricaoEstadual;
+    }
+
+    public void setInscricaoEstadual(String inscricaoEstadual) {
+        this.inscricaoEstadual = inscricaoEstadual;
+    }
+
+    public String getInscricaoMunicipall() {
+        return inscricaoMunicipall;
+    }
+
+    public void setInscricaoMunicipall(String inscricaoMunicipall) {
+        this.inscricaoMunicipall = inscricaoMunicipall;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
