2874a321d3598c785fd0fbc08c0cbb52509fb7e9
Diego Oliveira <diegodsoliveira@gmail.com>
2022-10-08 19:41:32 -0300

    primeiro teste

--- backend/src/test/java/com/diego/lojavirtual/LojaVirtualApplicationTests.java
+++ backend/src/test/java/com/diego/lojavirtual/LojaVirtualApplicationTests.java
@@ -1,11 +1,24 @@
 package com.diego.lojavirtual;
 
+import com.diego.lojavirtual.model.Acesso;
+import com.diego.lojavirtual.service.AcessoService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest()
 class LojaVirtualApplicationTests {
 
+	@Autowired
+	private AcessoService acessoService;
+
+	@Test
+	public void testaCadastroAcesso() {
+		Acesso acesso = new Acesso();
+
+		acesso.setDescricao("ROLE_ADMIN");
+		acessoService.create(acesso);
+	}
 	@Test
 	void contextLoads() {
 	}
