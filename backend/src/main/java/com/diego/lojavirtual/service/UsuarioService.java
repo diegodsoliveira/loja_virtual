c1d1c86a7d934e217751a5cb61aa9103db9d7edc
Diego Oliveira <diegodsoliveira@gmail.com>
2022-10-08 19:59:32 -0300

    usuario service

---
+++ backend/src/main/java/com/diego/lojavirtual/service/UsuarioService.java
@@ -0,0 +1,12 @@
+package com.diego.lojavirtual.service;
+
+import com.diego.lojavirtual.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+}
