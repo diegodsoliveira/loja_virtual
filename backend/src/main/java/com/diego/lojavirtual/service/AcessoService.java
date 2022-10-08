f55c6f620cef277d93e82114937c23b444673fb8
Diego Oliveira <diegodsoliveira@gmail.com>
2022-10-08 19:31:49 -0300

    first service

---
+++ backend/src/main/java/com/diego/lojavirtual/service/AcessoService.java
@@ -0,0 +1,18 @@
+package com.diego.lojavirtual.service;
+
+import com.diego.lojavirtual.model.Acesso;
+import com.diego.lojavirtual.repository.AcessoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AcessoService {
+
+    @Autowired
+    private AcessoRepository acessoRepository;
+
+    public Acesso create(Acesso obj) {
+        obj.setId(null);
+        return acessoRepository.save(obj);
+    }
+}
